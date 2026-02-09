0a7b9c4b42ea71a36f6418e54ab968e0e7c0c3bd
neelaksh <Neelaksh.gupta05@gmail.com>
2026-02-09 15:43:17 -0500

    user helper to inject extracted user infor from JWT token

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,25 @@
+import { getDataFromToken } from "@/helpers/getDataFromToken";
+import { NextRequest, NextResponse } from "next/server";
+
+import {connect} from"@/dbConfig/dbConfig"
+import User from "@/models/userModel";
+
+connect();
+
+export async function GET(request: NextRequest) {
+    try {
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({_id: userId}).
+        select("-password")
+
+        return NextResponse.json({
+            messsage: "user found",
+            data: user
+        })
+    } catch (error: any) {
+        return NextResponse.json(
+            { error: error.message },
+            { status: 400 }
+        );
+    }
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,16 @@
 "use client"
 import axios from "axios"
+import Link from "next/link"
 import { useRouter } from "next/navigation"
-import React from "react"
+import React, { useState } from "react"
 import toast from "react-hot-toast"
 
+
+//update so we use the hook useEffect to update as soon as the request reaches the profile page
 export default function ProfilePage() {
 
     const router = useRouter()
+    const [data,setData] = useState("nothing")
     
     //initiating the logout up
     const logout = async () => {
@@ -20,12 +24,26 @@ export default function ProfilePage() {
             } 
     }
 
+    const getUserDetails = async () => {
+        const res = await axios.get('/api/users/me')
+        setData(res.data.data.username)
+        console.log()
+    }
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
             <hr />
             <p>Profile page</p>
+            <h2 className="p-3 rounded bg-green-500">{data === "nothing" ? "Nothing" : <Link href={`/profile/${data}`}>{data}</Link>}</h2>
             <hr />
+
+            <button 
+            className = "bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+            onClick={getUserDetails}
+            >
+                Get User details
+            </button>
 
             <button 
             className = "bg-blue-500 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
