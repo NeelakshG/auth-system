1beec44c28d07bd7c2adc13fd932643601a86800
neelaksh <Neelaksh.gupta05@gmail.com>
2026-02-09 02:32:35 -0500

    add login flow: submit form, validate, go to temporary profile page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,10 @@
 "use client" // This component runs in the browser, not on the server
 import Link from "next/link"
-import React from "react"
+import React, { useEffect } from "react"
 import { useRouter } from "next/navigation"
-import { Axios } from "axios"
+import axios, { Axios } from "axios"
+import { error } from 'console';
+import toast from "react-hot-toast"
 
 
 
@@ -14,13 +16,38 @@ export default function LoginPage() {
         password: "",
     })
 
-    const onLogin = () => {
+    const router = useRouter()
+    const [buttonDisabled, setButtonDisabled] = React.useState(false)
+    const [loading, setLoading] = React.useState(false)
+
+    const onLogin = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.post("/api/users/login", user)
+            console.log("login success", response.data)
+            router.push("/profile")
+        } catch (error: any) {
+            console.log("Login failed", error.message)
+            toast.error(error.message)
+        } finally {
+            setLoading(false)
+        }
 
     }
 
+    useEffect(() => {
+            if (user.email.length > 0 && user.password.length > 0 ) {
+                setButtonDisabled(false)
+            } else {
+                setButtonDisabled(true)
+            }
+        }, [user])
+
+    
+
     return (
         <div className= "flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Login</h1>
+            <h1>{loading? "Processing" : "Login in"}</h1>
             <hr />
 
             <label htmlFor="email">email</label>
