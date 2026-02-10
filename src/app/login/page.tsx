20d9facf5c76fbbc3345d645e23e2fc5f099a095
neelaksh <Neelaksh.gupta05@gmail.com>
2026-02-09 20:09:24 -0500

    cleaning up layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,7 +2,8 @@
 import Link from "next/link"
 import React, { useEffect } from "react"
 import { useRouter } from "next/navigation"
-import axios from "axios"
+import axios, { Axios } from "axios"
+import { error } from 'console';
 import toast from "react-hot-toast"
 
 
